2899dc5780cdb8f6dc2293b2175972c253a3b1f4
brunolepis <brunka215@gmail.com>
2024-06-18 16:15:40 +0200

    refactor: fix image not displaying properly on specified theme

--- src/components/landing/showcase-image.tsx
+++ src/components/landing/showcase-image.tsx
@@ -1,26 +1,33 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 import ShowcaseDark from "../../../public/showcase-dark.png";
 import ShowcaseLight from "../../../public/showcase-light.png";
 
 export function ShowcaseImage() {
-  const { resolvedTheme } = useTheme();
-
   return (
     <div className="mx-auto mt-12 max-w-5xl">
-      <div className="relative overflow-hidden rounded-2xl">
+      <div className="relative rounded-2xl">
         <div className="absolute bottom-0 left-0 right-0 z-10 h-full bg-gradient-to-t from-white dark:from-zinc-950" />
-        <Image
-          className="aspect-video w-full rounded-[inherit] border border-zinc-100 dark:border-zinc-800"
-          src={resolvedTheme === "light" ? ShowcaseLight : ShowcaseDark}
-          alt="Showcase"
-          width={1920}
-          height={1080}
-          draggable={false}
-        />
+        <div className="aspect-video w-full overflow-hidden rounded-[inherit] border border-zinc-100 dark:border-zinc-800">
+          <Image
+            className="hidden size-full dark:inline"
+            src={ShowcaseDark}
+            alt="Dark mode showcase image"
+            width={1920}
+            height={1080}
+            priority
+          />
+          <Image
+            className="inline size-full dark:hidden"
+            src={ShowcaseLight}
+            alt="Light mode showcase image"
+            width={1920}
+            height={1080}
+            priority
+          />
+        </div>
       </div>
     </div>
   );
