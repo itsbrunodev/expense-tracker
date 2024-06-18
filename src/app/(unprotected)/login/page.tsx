9b4255a0e761e7e0ec765760e537b11ce2be0370
brunolepis <brunka215@gmail.com>
2024-06-18 16:12:53 +0200

    chore: remove unnecessary code, add warning

--- src/app/(unprotected)/login/page.tsx
+++ src/app/(unprotected)/login/page.tsx
@@ -1,3 +1,4 @@
+import { CircleAlertIcon } from "lucide-react";
 import Link from "next/link";
 
 import { Logo } from "@/components/logo";
@@ -8,7 +9,7 @@ import { signIn } from "@/lib/actions/auth";
 export default function Page() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <div className="space-y-6 text-center">
+      <div className="max-w-80 space-y-6 text-center">
         <Link href="/">
           <Logo className="mx-auto size-12" />
         </Link>
@@ -19,25 +20,22 @@ export default function Page() {
           </p>
         </div>
         <form
-          className="flex min-w-80 flex-col gap-6 rounded-xl border border-zinc-100 bg-zinc-50 p-6 dark:border-zinc-800 dark:bg-zinc-900"
+          className="flex flex-col gap-6 rounded-xl border border-zinc-100 bg-zinc-50 p-6 dark:border-zinc-800 dark:bg-zinc-900"
           action={signIn}
         >
-          {/* <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <Input placeholder="name@example.com" />
-              <Input placeholder="••••••" />
-            </div>
-            <div></div>
-          </div> */}
           <div className="flex flex-col gap-2">
-            {/* <Button variant="secondary" name="google">
-              Continue with Google
-            </Button> */}
             <Button variant="default" name="github">
               Continue with GitHub
             </Button>
           </div>
         </form>
+        <div className="space-y-2 rounded-xl border border-amber-500 bg-amber-500/20 p-6 text-left text-amber-700 dark:border-amber-600 dark:bg-amber-600/20 dark:text-amber-300">
+          <h1 className="font-semibold">Warning</h1>
+          <p className="text-sm">
+            This project is still being actively worked on and is not fully
+            ready for production. Expect <b>bugs</b> and <b>missing features</b>.
+          </p>
+        </div>
       </div>
     </div>
   );
