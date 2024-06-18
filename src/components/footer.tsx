4c9fc0f86c976a9bbde0e230478260eabc675c38
brunolepis <brunka215@gmail.com>
2024-06-18 16:13:12 +0200

    chore: footer better coloring

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
-import { ThemeToggle } from "./theme/theme-toggle";
+import { ThemeSwitcher } from "./theme-switcher";
 
 export const dynamic = "force-dynamic";
 
@@ -16,40 +16,28 @@ export function Footer({ className }: { className?: string }) {
   return (
     <footer
       className={cn(
-        "mx-auto mt-16 flex w-full max-w-4xl items-center justify-between border-t border-t-zinc-50 px-4 py-4 text-sm text-zinc-700 dark:border-t-zinc-900 dark:text-zinc-200 xl:px-0",
+        "mx-auto mt-16 flex w-full max-w-4xl items-center justify-between border-t border-t-zinc-50 px-4 py-4 text-sm dark:border-t-zinc-900 xl:px-0",
         className
       )}
     >
-      <p className="flex items-center font-medium">
+      <p className="flex items-center font-medium text-zinc-600 dark:text-zinc-100">
         <Logo className="pointer-events-none mr-2 size-3.5 fill-zinc-700 dark:fill-zinc-200" />
-        <span>
-          Expense Tracker{" "}
-          <span className="font-normal">
-            {t.rich("by", {
-              a: (text) => (
-                <Link
-                  className="underline transition-colors hover:text-zinc-700 dark:hover:text-white"
-                  href="https://itsbruno.dev/"
-                  target="_blank"
-                >
-                  {text}
-                </Link>
-              ),
-            })}{" "}
-          </span>
+        Expense Tracker
+        <span className="ml-1 font-normal text-zinc-700 dark:text-zinc-200">
+          {t.rich("by", {
+            a: (text) => (
+              <Link
+                className="underline transition-colors hover:text-zinc-700 dark:hover:text-white"
+                href="https://itsbruno.dev/"
+                target="_blank"
+              >
+                {text}
+              </Link>
+            ),
+          })}
         </span>
       </p>
-      <ThemeToggle />
-      {/* <p>
-        Made by{" "}
-        <Link
-          className="underline transition-colors hover:text-zinc-700 dark:hover:text-white"
-          href="https://itsbruno.dev/"
-          target="_blank"
-        >
-          Bruno
-        </Link>
-      </p> */}
+      <ThemeSwitcher />
     </footer>
   );
 }
