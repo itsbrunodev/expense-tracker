dccf1c87cb975a103ef26356f34f8b10484e2469
brunolepis <brunka215@gmail.com>
2024-06-19 15:50:02 +0200

    refactor: better responsive design for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { ShowcaseImage } from "@/components/landing/showcase-image";
 import { Navbar } from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { FEATURES } from "@/lib/constants";
+import { cn } from "@/lib/utils";
 
 export default async function Page() {
   const t = await getTranslations();
@@ -15,7 +16,7 @@ export default async function Page() {
     <div className="h-full w-full">
       <Navbar className="mb-0 max-w-5xl" />
       <div className="mx-auto flex min-h-[500px] w-full max-w-5xl flex-col items-center justify-center gap-8 px-4 text-center xl:px-0">
-        <h1 className="max-w-3xl text-5xl font-semibold leading-tight">
+        <h1 className="max-w-3xl text-4xl font-semibold leading-tight md:text-5xl">
           {t.rich("landing.heading", {
             a: (text) => (
               <span className="bg-gradient-to-b from-zinc-500 to-zinc-900 bg-clip-text text-transparent dark:from-white dark:to-zinc-300">
@@ -29,7 +30,7 @@ export default async function Page() {
             ),
           })}
         </h1>
-        <p className="max-w-xl text-lg leading-normal text-zinc-900 dark:text-zinc-50">
+        <p className="max-w-xl leading-normal text-zinc-900 dark:text-zinc-50 md:text-lg">
           {t("landing.subheading")}
         </p>
         <Button className="h-fit px-4 py-2" asChild>
@@ -50,21 +51,29 @@ export default async function Page() {
           <h2 className="text-center text-3xl font-medium">
             {t("landing.features")}
           </h2>
-          <div className="grid grid-cols-3 gap-12">
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
             {FEATURES.map((feature, i) => (
               <div
-                className="group flex flex-col items-start justify-start gap-2 transition-all"
+                className={cn(
+                  "group flex flex-col justify-start gap-2 transition-all md:items-start md:text-left",
+                  i % 2 && "items-end text-right"
+                )}
                 key={i}
               >
-                <div className="flex h-14 items-center gap-2">
+                <div
+                  className={cn(
+                    "flex h-14 items-center gap-2 md:flex-row",
+                    i % 2 && "flex-row-reverse"
+                  )}
+                >
                   <div className="rounded-md bg-gradient-to-b from-zinc-50 to-zinc-50/20 p-2.5 text-zinc-800 group-hover:from-green-100 group-hover:to-green-100/20 group-hover:text-green-800 dark:from-zinc-800 dark:to-zinc-800/20 dark:text-zinc-100 group-hover:dark:from-green-800 group-hover:dark:to-green-800/20 group-hover:dark:text-green-200">
                     <feature.icon className="size-8" strokeWidth={1} />
                   </div>
-                  <h3 className="text-lg font-medium text-zinc-800 dark:text-zinc-50">
+                  <h3 className="text-lg font-medium text-zinc-800 dark:text-zinc-100">
                     {t(feature.title)}
                   </h3>
                 </div>
-                <p className="text-sm text-zinc-800 dark:text-zinc-50">
+                <p className="text-sm text-zinc-800 dark:text-zinc-200">
                   {t(feature.description)}
                 </p>
               </div>
@@ -72,10 +81,12 @@ export default async function Page() {
           </div>
         </div>
         <div className="mx-auto max-w-5xl px-4 xl:px-0">
-          <div className="flex h-96 w-full flex-col items-center justify-center gap-6 rounded-2xl bg-gradient-to-b from-green-400 to-green-700 text-center dark:from-green-600 dark:to-green-950">
-            <h1 className="text-4xl font-bold">{t("landing.ready")}</h1>
+          <div className="flex h-96 w-full flex-col items-center justify-center gap-6 rounded-2xl bg-gradient-to-b from-green-400 to-green-700 px-4 text-center dark:from-green-600 dark:to-green-950 md:px-0">
+            <h1 className="text-3xl font-bold md:text-4xl">
+              {t("landing.ready")}
+            </h1>
             <div className="flex max-w-[550px] flex-col items-center justify-center gap-3">
-              <p className="text-lg text-zinc-800 dark:text-zinc-50">
+              <p className="text-zinc-800 dark:text-zinc-50 md:text-lg">
                 {t("landing.subheading")}
               </p>
               <Button className="h-fit px-4 py-2" asChild>
